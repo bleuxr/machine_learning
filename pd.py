26a82cbd86beece0321a3da6416fa068ea749f87
bleuxr <38353817+bleuxr@users.noreply.github.com>
2018-07-29 00:56:38 +0800

    add another example of display

--- pd.py
+++ pd.py
@@ -4,4 +4,5 @@ import pandas as pd
 
 data={'Name':["jhon","Anna","Peter","Linda"],'Location':["New York","Paris","Berlin","London"],'Age':[24,13,53,33]}
 data_pandas=pd.DataFrame(data)
-display(data_pandas)
+#display(data_pandas)
+display(data_pandas[data_pandas.Age>30])
