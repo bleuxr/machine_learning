e366c8043b1dc8821983d0fd66d0faf5f865c5f7
bleuxr <38353817+bleuxr@users.noreply.github.com>
2018-08-05 16:52:03 +0800

    use knn

--- iris.py
+++ iris.py
@@ -39,3 +39,27 @@ print("y_test shape:{}".format(y_test.shape))
 
 iris_dataframe=pd.DataFrame(X_train,columns=iris_dataset.feature_names)
 grr=pd.scatter_matrix(iris_dataframe,c=y_train,figsize=(15,15),marker='0',hist_kwds={'bins':20},s=60,alpha=.8,cmap=mglearn.cm3)
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import mglearn
+from sklearn.datasets import load_iris
+
+from sklearn.model_selection import train_test_split
+from pandas.plotting import scatter_matrix
+from sklearn.neighbors import KNeighborsClassifier
+
+iris_dataset = load_iris()
+X_train,X_test,y_train,y_test=train_test_split(iris_dataset['data'],iris_dataset['target'],random_state=0)
+
+knn=KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train,y_train)
+
+X_new=np.array([[5,2.9,1,0.2]])
+print("X_new.shape:{}".format(X_new.shape))
+
+prediction=knn.predict(X_new)
+print("Prediction:{}".format(prediction))
+print("Predicted target name:{}".format(iris_dataset['target_names'][prediction]))
