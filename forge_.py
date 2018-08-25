c95ee309b24b3f499117f17888ee3276dbe1831d
bleuxr <38353817+bleuxr@users.noreply.github.com>
2018-08-25 23:44:43 +0800

    add knn model

--- forge_.py
+++ forge_.py
@@ -102,3 +102,65 @@ for n_neighbors, ax in zip([1,3,9],axes):
     #plt.ylabel("feature 1")
 
 axes[0].legend(loc=3)
+
+#%%
+import numpy as np
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+import mglearn
+from sklearn.model_selection import train_test_split
+from sklearn.datasets import load_breast_cancer
+from sklearn.neighbors import KNeighborsClassifier
+
+cancer =load_breast_cancer()
+X_train,X_test,y_train,y_test=train_test_split(cancer.data,cancer.target,stratify=cancer.target,random_state=66)
+
+training_accuracy=[]
+test_accuracy=[]
+
+neighbors_settings=range(1,11)
+
+for n_neighbors in neighbors_settings:
+    clf=KNeighborsClassifier(n_neighbors=n_neighbors)
+    clf.fit(X_train,y_train)
+
+    training_accuracy.append(clf.score(X_train,y_train))
+    test_accuracy.append(clf.score(X_test,y_test))
+
+plt.plot(neighbors_settings,training_accuracy,label="training accuracy")
+plt.plot(neighbors_settings,test_accuracy,label="test accuracy")
+plt.ylabel("Accuracy")
+plt.xlabel("n_neighbors")
+plt.legend()
+
+#%%
+import numpy as np
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+import mglearn
+
+#mglearn.plots.plot_knn_regression(n_neighbors=1)
+mglearn.plots.plot_knn_regression(n_neighbors=3)
+
+#%%
+import numpy as np
+import matplotlib
+import pandas as pd
+import matplotlib.pyplot as plt
+import mglearn
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+
+X,y=mglearn.datasets.make_wave(n_samples=40)
+
+X_train,X_test,y_train,y_test=train_test_split(X,y,random_state=0)
+
+reg=KNeighborsRegressor(n_neighbors=3)
+
+reg.fit(X_train,y_train)
+
+print("Test set prediction:\n{}".format(reg.predict(X_test)))
+
+print("Test set R^2:{:.2f}".format(reg.score(X_test,y_test)))
